166784200305ccecf2302a9f378fa3b4d1daaa41
davidwant <ccgkwyx@hotmail.com>
2014-08-01 21:08:56 +0800

    remove xcode project file

---
+++ Refactoring/ProductLineFactory.cpp
@@ -0,0 +1,9 @@
+//
+//  ProductLineFactory.cpp
+//  Refactoring
+//
+//  Created by davidw on 7/25/14.
+//  Copyright (c) 2014 davidw. All rights reserved.
+//
+
+#include "ProductLineFactory.h"
---
+++ Refactoring/ProductLineFactory.h
@@ -0,0 +1,14 @@
+//
+//  ProductLineFactory.h
+//  Refactoring
+//
+//  Created by davidw on 7/25/14.
+//  Copyright (c) 2014 davidw. All rights reserved.
+//
+
+#ifndef __Refactoring__ProductLineFactory__
+#define __Refactoring__ProductLineFactory__
+
+#include <iostream>
+
+#endif /* defined(__Refactoring__ProductLineFactory__) */
